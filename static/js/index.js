8683b47b873c7dfbb2549366ed586e1b8c1470b1
JeremyBrent <jeremybrentdata@gmail.com>
2020-07-04 02:55:32 -0400

    working on interactive marker map

--- static/js/index.js
+++ static/js/index.js
@@ -15,18 +15,18 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-// function dropDownSelect(dropDown, dropDownValue) {
-//   var dropDownValue = dropDown.property("value");
-//   return dropDownValue
+// function dropDownSelect(dropDown) {
+//   dropDown.on("change", () => {
+//     var dropDownValue = dropDown.property("value");
+//     return dropDownValue;
+//   });
 // }
 
 // Function to filter data based on fuel type
-function filter(dataset, energyType) {
+function filterByEnergy(dataset, energyType) {
   var statsData = dataset.filter((data) => data.primary_fuel == energyType);
   return statsData;
 }
-
-
 
 // var stateSelectValue;
 // locationStateSelect.on("change", dropDownSelect(locationStateSelect, stateSelectValue));
@@ -37,7 +37,7 @@ locationStateSelect.on("change", () => {
 });
 
 locationEnergySelect.on("change", () => {
-  var energySelectValue = locationEnergySelect.node().value;
+  energySelectValue = locationEnergySelect.node().value;
   console.log(energySelectValue);
 });
 
@@ -62,19 +62,8 @@ prodYearSelect.on("change", () => {
 });
 
 //////////
-// Data extraction for maps 
+// Data extraction maps filterers
 d3.csv(dataset).then((data) => {
-  console.log(data);
-
-  data.forEach((d) => {
-    d.capacity_mw = +d.capacity_mw;
-    d.commissioning_year = +d.commissioning_year;
-    d.generation_gwh_2017 = +d.generation_gwh_2017;
-    d.id = +d.id;
-    d.wepp_id = +d.wepp_id;
-    d.year_of_capacity_data = +d.year_of_capacity_data;
-  });
-
   var difYears = [
     ...new Set(data.map((x) => x.commissioning_year).sort((a, b) => b - a)),
   ];
@@ -102,35 +91,95 @@ d3.csv(dataset).then((data) => {
       .attr("value", `${difEnergy[i]}`)
       .text(difEnergy[i]);
   }
-
-  // console.log(data)
-  // // Create a new marker cluster group
-  var markerClusterGroup = L.markerClusterGroup();
-
-  // // Loop through data
-  for (var i = 0; i < data.length; i++) {
-    // Set the data location property to a variable
-    var location = [data[i].latitude, data[i].longitude];
-
-    // Check for location property
-    if (location) {
-      // Add a new marker to the cluster group and bind a pop-up
-      markerClusterGroup.addLayer(
-        L.marker(location).bindPopup(`
-              <p><strong> Name: </strong> ${data[i].name} </p>
-              <hr>
-              <p><strong> Commission Year: </strong> ${data[i].commissioning_year} </p>
-              <p><strong> Primary Fuel Type: </strong> ${data[i].primary_fuel} </p>
-            `)
-      );
-    }
-  }
-  // Add our marker cluster layer to the map
-  markerClusterGroup.addTo(myMap);
 });
 
 //////////
-// Data extraction to build the table
+// Data extraction for maps
+d3.csv(dataset).then((data) => {
+  var columnNames = [
+    "name",
+    "primary_fuel",
+    "commissioning_year",
+    "latitude",
+    "longitude",
+  ];
+
+  const redux = (array) =>
+    array.map((o) =>
+      columnNames.reduce((acc, curr) => {
+        acc[curr] = o[curr];
+        return acc;
+      }, {})
+    );
+
+  var reducedData = redux(data);
+
+  reducedData.forEach((d) => {
+    d.latitude = +d.latitude;
+    d.longitude = +d.longitude;
+    d.commissioning_year = +d.commissioning_year;
+  });
+
+  var markerClusterGroup = L.markerClusterGroup();
+
+  for (var i = 0; i < reducedData.length; i++) {
+    allLocations = [reducedData[i].latitude, reducedData[i].longitude];
+
+    markerClusterGroup.addLayer(
+      L.marker(allLocations).bindPopup(`
+          <p><strong> Name: </strong> ${reducedData[i].name} </p>
+          <hr>
+          <p><strong> Commission Year: </strong> ${reducedData[i].commissioning_year} </p>
+          <p><strong> Primary Fuel Type: </strong> ${reducedData[i].primary_fuel} </p>
+        `)
+    );
+  }
+  // Add our marker cluster layer to the map
+  markerClusterGroup.addTo(myMap);
+
+  locationEnergySelect.on("change", () => {
+    var energySelectValue = locationEnergySelect.node().value;
+    var filteredData = filterByEnergy(reducedData, energySelectValue);
+
+    // Check for location property
+    if (energySelectValue == "All") {
+      for (var i = 0; i < reducedData.length; i++) {
+        allLocations = [reducedData[i].latitude, reducedData[i].longitude];
+
+        markerClusterGroup.addLayer(
+          L.marker(allLocations).bindPopup(`
+                <p><strong> Name: </strong> ${reducedData[i].name} </p>
+                <hr>
+                <p><strong> Commission Year: </strong> ${reducedData[i].commissioning_year} </p>
+                <p><strong> Primary Fuel Type: </strong> ${reducedData[i].primary_fuel} </p>
+              `)
+        );
+      }
+    } else {
+      // // Loop through data
+      for (var i = 0; i < filteredData.length; i++) {
+        // Set the data location property to a variable
+        var filteredLocations = [
+          filteredData[i].latitude,
+          filteredData[i].longitude,
+        ];
+        markerClusterGroup.addLayer(
+          L.marker(filteredLocations).bindPopup(`
+              <p><strong> Name: </strong> ${filteredData[i].name} </p>
+              <hr>
+              <p><strong> Commission Year: </strong> ${filteredData[i].commissioning_year} </p>
+              <p><strong> Primary Fuel Type: </strong> ${filteredData[i].primary_fuel} </p>
+            `)
+        );
+      }
+    }
+    // Add our marker cluster layer to the map
+    markerClusterGroup.addTo(myMap);
+  });
+});
+
+//////////
+// Data extraction to build the data table
 d3.csv(dataset).then((data) => {
   var columnNames = [
     "name",
@@ -144,13 +193,12 @@ d3.csv(dataset).then((data) => {
 
   // Function to reduce data object to specific properties
   const redux = (array) =>
-  array.map((o) =>
-    columnNames.reduce((acc, curr) => {
-      acc[curr] = o[curr];
-      return acc;
-    }, {})
-  );
-
+    array.map((o) =>
+      columnNames.reduce((acc, curr) => {
+        acc[curr] = o[curr];
+        return acc;
+      }, {})
+    );
 
   var almostTableData = redux(data);
 
@@ -172,8 +220,6 @@ d3.csv(dataset).then((data) => {
   });
 });
 
-
-
 function quantity(dataset) {
   var total = dataset.length;
   return total;
@@ -182,34 +228,28 @@ function quantity(dataset) {
 //////////
 // Data extraction for stats table
 d3.csv(dataset).then((data) => {
-
-  var columnNames = [
-    "primary_fuel",
-    "generation_gwh_2017",
-  ];
+  var columnNames = ["primary_fuel", "generation_gwh_2017"];
 
   const redux = (array) =>
-  array.map((o) =>
-    columnNames.reduce((acc, curr) => {
-      acc[curr] = o[curr];
-      return acc;
-    }, {})
-  );
+    array.map((o) =>
+      columnNames.reduce((acc, curr) => {
+        acc[curr] = o[curr];
+        return acc;
+      }, {})
+    );
 
   var almostTableData = redux(data);
-  
+
   almostTableData.forEach((d) => {
     d.generation_gwh_2017 = +d.generation_gwh_2017;
   });
 
-  console.log(almostTableData)
-
-  
+  console.log(almostTableData);
 
   var difEnergy = [...new Set(data.map((x) => x.primary_fuel).sort())];
 
-  var dropDown = d3.select(".energyTypeStats")
-  var tbody = d3.select(".stats-table-body tr")
+  var dropDown = d3.select(".energyTypeStats");
+  var tbody = d3.select(".stats-table-body tr");
 
   for (var i = 0; i < difEnergy.length; i++) {
     dropDown
@@ -220,29 +260,29 @@ d3.csv(dataset).then((data) => {
 
   var sum = 0;
 
-  almostTableData.forEach(function(item){
+  almostTableData.forEach(function (item) {
     sum += item.generation_gwh_2017;
-  })
+  });
 
   var allStationsSum = sum.toFixed(2);
   var allStationsCount = data.length;
   var allStationsAvg = (allStationsSum / allStationsCount).toFixed(2);
-  
-  tbody.append('td').text(numberWithCommas(allStationsCount));
+
+  tbody.append("td").text(numberWithCommas(allStationsCount));
   tbody.append("td").text(numberWithCommas(allStationsSum));
   tbody.append("td").text(numberWithCommas(allStationsAvg));
 
   dropDown.on("change", () => {
-    tbody.selectAll("td").remove()
+    tbody.selectAll("td").remove();
     var value = dropDown.property("value");
-    var dataStats = filter(almostTableData, value);
+    var dataStats = filterByEnergy(almostTableData, value);
     var dataStationsCount = dataStats.length;
     var sum = 0;
-    dataStats.forEach((item)=> sum += item.generation_gwh_2017);
+    dataStats.forEach((item) => (sum += item.generation_gwh_2017));
     var dataStationsSum = sum.toFixed(2);
     var dataStationsAvg = (dataStationsSum / dataStationsCount).toFixed(2);
     if (value == "All") {
-      tbody.append('td').text(numberWithCommas(allStationsCountComma));
+      tbody.append("td").text(numberWithCommas(allStationsCountComma));
       tbody.append("td").text(numberWithCommas(allStationsSumComma));
       tbody.append("td").text(numberWithCommas(allStationsAvg));
     } else {
@@ -250,8 +290,7 @@ d3.csv(dataset).then((data) => {
       tbody.append("td").text(numberWithCommas(dataStationsSum));
       tbody.append("td").text(numberWithCommas(dataStationsAvg));
     }
-  })
-
+  });
 });
 
 // Map 1
