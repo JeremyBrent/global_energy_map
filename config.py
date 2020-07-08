c994f06d8ee0eeb40d0290e6452d70c376ad7d53
JeremyBrent <jeremybrentdata@gmail.com>
2020-07-08 18:56:31 -0400

    un-ignored config file so we can deploy on heroku

---
+++ config.py
@@ -0,0 +1,4 @@
+username_heroku = "edpkomrjtjquyg"
+password_heroku = "426cdf768f54226d68000a109cb359f038368bab5f8cd02e25219e43ab5f45c6"
+host_heroku = "ec2-34-230-231-71.compute-1.amazonaws.com"
+database_heroku = "d1atsfvk7j6fiu"
